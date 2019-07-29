30c95c7ffe695b4845c42bf80ec326337a4758da
Kris Ezra <kris.ezra@gmail.com>
2019-07-29 16:40:31 -0400

    Updated install file

--- installZsh.sh
+++ installZsh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 pacman -S zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -7,4 +6,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # Set up Zsh auto suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 
-# CP git-auto-status file into ~/.oh-my-zsh/plugins/git-auto-status/git-auto-status.plugin.zsh
+ln -sf ~/.shell/zshrc ~/.zshrc
+
+mkdir -p ~/.oh-my-zsh/plugins/git-auto-status
+ln -sf ~/.shell/git-auto-status.plugin.zsh ~/.oh-my-zsh/plugins/git-auto-status/git-auto-status.plugin.zsh
+
+ln -sf ~/.shell/kris.zsh-theme ~/.oh-my-zsh/themes/kris.zsh-theme
