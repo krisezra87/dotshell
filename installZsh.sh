c518f3f3006233c2fa2992a7fb8e8231bc453002
Kris Ezra <kris.ezra@gmail.com>
2019-08-17 11:09:46 -0400

    breaking up files for better sequential run

--- installZsh.sh
+++ installZsh.sh
@@ -27,17 +27,4 @@ if $FLAG; then
     rmdir $DIR
 fi
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Set up Zsh auto suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-
-ln -sf ~/.shell/zsh/zshrc ~/.zshrc
-
-mkdir -p ~/.oh-my-zsh/plugins/git-auto-status
-ln -sf ~/.shell/zsh/git-auto-status.plugin.zsh ~/.oh-my-zsh/plugins/git-auto-status/git-auto-status.plugin.zsh
-
-ln -sf ~/.shell/zsh/kris.zsh-theme ~/.oh-my-zsh/themes/kris.zsh-theme
-
-# Install tmux
-bash ~/.shell/installTmux.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" ; ~/.shell/configureZsh.sh ; ~/.shell/installTmux.sh
