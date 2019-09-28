92d08f8120ed222d1858f9326bd5a564a7425ec8
Kris Ezra <kris.ezra@gmail.com>
2019-09-27 22:50:48 -0400

    Added zsh setup for fzf

--- zsh/configureZsh.sh
+++ zsh/configureZsh.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
 ln -sf ~/.shell/zsh/zshrc ~/.zshrc
+ln -sf ~/.shell/zsh/.fzf.zsh ~/.fzf.zsh
 
 mkdir -p ~/.oh-my-zsh/plugins/git-auto-status
 ln -sf ~/.shell/zsh/git-auto-status.plugin.zsh ~/.oh-my-zsh/plugins/git-auto-status/git-auto-status.plugin.zsh
