6223c6aa1216022e15b82c6a16c3cf2ce549f119
Kris Ezra <kris.ezra@gmail.com>
2020-09-12 18:02:04 -0400

    Made .fzf config more portable

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/kezra/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/kezra/.fzf/bin"
+if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/kezra/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/kezra/.fzf/shell/key-bindings.zsh"
+source "/home/$USER/.fzf/shell/key-bindings.zsh"
 
 # Add a default command to use ripgrep
 export FZF_DEFAULT_COMMAND='rg --files --hidden'
