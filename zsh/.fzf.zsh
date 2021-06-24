6b9c024ab076ecf443cc34acf672bc0d61f19096
Kris Ezra <kris.ezra@gmail.com>
2021-06-24 17:07:27 -0400

    Switched to nvim

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
+if [[ ! "$PATH" == */home/$USER/.local/share/nvim/site/pack/packer/start/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/$USER/.local/share/nvim/site/pack/packer/start/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USER/.local/share/nvim/site/pack/packer/start/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/$USER/.fzf/shell/key-bindings.zsh"
+source "/home/$USER/.local/share/nvim/site/pack/packer/start/fzf/shell/key-bindings.zsh"
 
 # Add a default command to use ripgrep
 export FZF_DEFAULT_COMMAND='rg --files --hidden'
