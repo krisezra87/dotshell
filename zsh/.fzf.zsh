33d299ffb73f8467587d32b2a436ccaf95dcd458
Kris Ezra <kris.ezra@gmail.com>
2026-05-22 12:32:45 -0400

    Updates to aliases pre claude investigation

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,16 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/$USER/.local/share/nvim/site/pack/packer/start/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/$USER/.local/share/nvim/site/pack/packer/start/fzf/bin"
+if [[ ! "$PATH" == */home/kezra/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/kezra/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/$USER/.local/share/nvim/site/pack/packer/start/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/home/$USER/.local/share/nvim/site/pack/packer/start/fzf/shell/key-bindings.zsh"
-
-# Add a default command to use ripgrep
-export FZF_DEFAULT_COMMAND='rg --files --hidden'
+source <(fzf --zsh)
